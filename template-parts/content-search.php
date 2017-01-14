7fb686d1a0197d7b7d73908f1818a1c33693f150
Mathilde Arconte <mathilde@m-arconte.fr>
2017-01-15 00:18:51 +0100

    Debug Bootstrap

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -4,7 +4,7 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package _s
+ * @package medusa
  */
 
 ?>
@@ -15,7 +15,7 @@
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php _s_posted_on(); ?>
+			<?php medusa_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
@@ -25,6 +25,6 @@
 	</div><!-- .entry-summary -->
 
 	<footer class="entry-footer">
-		<?php _s_entry_footer(); ?>
+		<?php medusa_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
