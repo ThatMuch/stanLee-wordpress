cadef394e21100d8526539053ce97d1b63daff9d
Mathilde Arconte <signup@marconte.fr>
2018-10-04 12:16:31 +0200

    fix: import acf plugin

--- inc/plugin-activation.php
+++ inc/plugin-activation.php
@@ -63,7 +63,7 @@ function Stanlee_register_required_plugins() {
  		array(
 			'name'               => 'Advanced Custom Fields PRO', // The plugin name.
 			'slug'               => 'advanced-custom-fields-pro', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . 'inc/plugins/acf.zip', // The plugin source.
+			'source'             => get_template_directory() . '/inc/plugins/acf.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '5.7.6', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
