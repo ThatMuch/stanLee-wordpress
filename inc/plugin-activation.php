1cb1496e0c55943e3d3902139334631ebac755b7
Mathilde Arconte <signup@marconte.fr>
2020-04-24 13:36:28 +0200

    remove update acf

--- inc/plugin-activation.php
+++ inc/plugin-activation.php
@@ -65,7 +65,7 @@ function stanlee_register_required_plugins() {
 			'slug'               => 'advanced-custom-fields-pro',                                               // The plugin slug (typically the folder name).
 			'source'             => get_template_directory() . '/inc/plugins/advanced-custom-fields-pro.zip',   // The plugin source.
 			'required'           => true,                                                                       // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '5.8.9',                                                                    // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'version'            => '5.7.7',                                                                    // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => true,                                                                       // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 		),
 		array(
