023ddcae3db8f53b094118ad44c2a9701199dacf
Mathilde Arconte <signup@marconte.fr>
2022-09-25 18:47:01 +0200

    removeBootstrap price

--- templates/section-price.php
+++ templates/section-price.php
@@ -23,7 +23,7 @@
         <div class="container">
             <!-- Title -->
             <?php if(get_sub_field('title') ) : ?>
-                  <h2 class="section__title text-center"><?php echo get_sub_field('title'); ?></h2>
+                  <h2 class="section__title"><?php echo get_sub_field('title'); ?></h2>
             <?php endif; ?>
             <!-- Title -->
             <!-- Count columns -->
@@ -34,50 +34,45 @@
             ?>
                   <?php if (have_rows('colonne') ) :
                   ?>
-            <div class="row justify-content-center">
-                        <?php while(have_rows('colonne')) : the_row();  ?>
-                        <?php if( get_sub_field('title') ) : ?>
-                        <div class="<?php echo ($count < 3) ? "col-sm-6" : "col-sm-4" ?> section-price__column">
-                              <div class="section-price__column__wrapper">
-                                    <!-- Title -->
-                                    <?php if( get_sub_field('title') ) : ?>
-                                        <h3 class="section-price__column__title text-center">  <?php echo get_sub_field('title'); ?> </h3>
-                                    <?php endif; ?>
-                                    <!-- Title -->
-                                    <!-- Price -->
-                                    <?php if( get_sub_field('prix') ) : ?>
-                                          <p class="section-price__column__price">
-                                                <!-- Devise -->
-                                                <?php if( get_sub_field('devise') ) : ?>
-                                                      <span class="section-price__column__devise"><?php echo get_sub_field('devise'); ?></span>
-                                                <?php endif; ?>
-                                                <!-- Devise -->
-                                                <?php echo get_sub_field('prix'); ?>
-                                          </p>
-                                    <?php endif; ?>
-                                    <!-- Price -->
-
-                                    <?php if ( have_rows('services') ) : ?>
-                                          <ul class="mb-auto">
-                                                <?php while (have_rows('services')) : the_row()?>
-                                                      <?php if( get_sub_field('text') ) : ?>
-                                                            <li class="section-price__column__service"><?php echo get_sub_field('text'); ?></li>
-                                                      <?php endif; ?>
-                                                <?php endwhile; ?>
-                                          </ul>
-                                    <?php endif; ?>
-
-        <!-- Button -->
-        <?php if (have_rows('button')) : ?>
-            <?php while ( have_rows('button') ) : the_row(); ?>
-                <?php if (get_sub_field('label') ) : ?>
-                    <a href="<?php the_sub_field('link'); ?>" class="btn btn-primary btn-block mt-2"><?php the_sub_field('label'); ?></a>
-                <?php endif; ?>
-            <?php endwhile; ?>
-        <?php endif; ?>
-        <!-- Button -->
-
-                              </div>
+            <div class="d-flex">
+			<?php while(have_rows('colonne')) : the_row();  ?>
+				<?php if( get_sub_field('title') ) : ?>
+                        	<div class="section-price__column">
+							<!-- Title -->
+							<?php if( get_sub_field('title') ) : ?>
+							<h3 class="section-price__column__title text-center">  <?php echo get_sub_field('title'); ?> </h3>
+							<?php endif; ?>
+							<!-- Title -->
+							<!-- Price -->
+							<?php if( get_sub_field('prix') ) : ?>
+								<p class="section-price__column__price">
+									<!-- Devise -->
+									<?php if( get_sub_field('devise') ) : ?>
+										<span class="section-price__column__devise"><?php echo get_sub_field('devise'); ?></span>
+									<?php endif; ?>
+									<!-- Devise -->
+									<?php echo get_sub_field('prix'); ?>
+								</p>
+							<?php endif; ?>
+							<!-- Price -->
+							<?php if ( have_rows('services') ) : ?>
+								<ul class="mb-auto">
+									<?php while (have_rows('services')) : the_row()?>
+										<?php if( get_sub_field('text') ) : ?>
+											<li class="section-price__column__service"><?php echo get_sub_field('text'); ?></li>
+										<?php endif; ?>
+									<?php endwhile; ?>
+								</ul>
+							<?php endif; ?>
+							<!-- Button -->
+							<?php if (have_rows('button')) : ?>
+								<?php while ( have_rows('button') ) : the_row(); ?>
+								<?php if (get_sub_field('label') ) : ?>
+									<a href="<?php the_sub_field('link'); ?>" class="btn btn-primary btn-block mt-2"><?php the_sub_field('label'); ?></a>
+								<?php endif; ?>
+								<?php endwhile; ?>
+							<?php endif; ?>
+							<!-- Button -->
                         </div>
                         <?php endif; ?>
                         <?php endwhile; ?>
